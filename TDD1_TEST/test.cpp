c93bd1b6b8ad25be5d1eaaa3fd986e6a2268e7ff
seungpyo.hong <hsp8405@naver.com>
2024-05-16 16:06:04 +0900

    한글 설정

--- TDD1_TEST/test.cpp
+++ TDD1_TEST/test.cpp
@@ -12,10 +12,8 @@ TEST_F(AccountFixture, TestName) {
 	EXPECT_EQ(calc.getBalance(), 10000);
 
 	calc.deposit(5000);
-
 	EXPECT_EQ(calc.getBalance(), 15000);
 
 	calc.withdraw(3000);
-
 	EXPECT_EQ(calc.getBalance(), 12000);
 }
